36093b826abc91a74033d868d0bc827c8e6ae521
Neil Ricci <iccir919@gmail.com>
2019-07-15 14:33:23 -0700

    Updates

---
+++ precache-manifest.6466101c58ab1aacf4d2866278b06ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63695d204a4f41d62491d5d857b4820",
+    "url": "/reduxify/index.html"
+  },
+  {
+    "revision": "35c642cb3beb5138921d",
+    "url": "/reduxify/static/css/2.6ba2bc4a.chunk.css"
+  },
+  {
+    "revision": "1de56ee11cea78d837e6",
+    "url": "/reduxify/static/css/main.9e60e691.chunk.css"
+  },
+  {
+    "revision": "35c642cb3beb5138921d",
+    "url": "/reduxify/static/js/2.550384a1.chunk.js"
+  },
+  {
+    "revision": "1de56ee11cea78d837e6",
+    "url": "/reduxify/static/js/main.385d3a07.chunk.js"
+  },
+  {
+    "revision": "9d611a47ecb471f1d012",
+    "url": "/reduxify/static/js/runtime~main.de91d61b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reduxify/precache-manifest.9b3dd61060427072377118a8fa381cf1.js"
+  "/reduxify/precache-manifest.6466101c58ab1aacf4d2866278b06ef5.js"
 );
 
 self.addEventListener('message', (event) => {
